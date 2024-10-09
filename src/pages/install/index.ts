d609aa1b4c771c6b3c4d334d3120e789958e1f87
Mohammed Elhaouari <m@codiume.com>
2024-10-09 17:53:50 +0100

    Add cloudflare adapter

---
+++ src/pages/install/index.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = ({ params, request }) => {
+	return new Response(
+		JSON.stringify({
+			path: new URL(request.url).pathname,
+		}),
+		{
+			headers: {
+				"Content-Type": "text/plain",
+			},
+		},
+	);
+};
