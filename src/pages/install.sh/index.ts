db1305e9c98595cbbc09173e79101cdf0ff0d508
Mohammed Elhaouari <m@codiume.com>
2024-10-10 17:11:20 +0100

    Enable hybrid mode for rendering

--- src/pages/install.sh/index.ts
+++ src/pages/install.sh/index.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from "astro";
 
 import content from "./_install.txt";
 
-export const GET: APIRoute = ({ request }) => {
+export const GET: APIRoute = () => {
   return new Response(content, {
     headers: {
       "Content-Type": "text/plain;charset=UTF-8",
