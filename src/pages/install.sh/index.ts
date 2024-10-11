2d06a19e235bbd84efa9e0fc1da93b0f44daccc8
Mohammed Elhaouari <m@codiume.com>
2024-10-11 23:44:23 +0100

    Add scripts to install php latest and composer

--- src/pages/install.sh/index.ts
+++ src/pages/install.sh/index.ts
@@ -3,9 +3,9 @@ import type { APIRoute } from "astro";
 import content from "./_install.txt";
 
 export const GET: APIRoute = () => {
-  return new Response(content, {
-    headers: {
-      "Content-Type": "text/plain;charset=UTF-8",
-    },
-  });
+	return new Response(content, {
+		headers: {
+			"Content-Type": "text/plain;charset=UTF-8",
+		},
+	});
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 };
