1a8732c1d417c409a21180ee2dd33c7ecfe6b2df
Mohammed Elhaouari <m@codiume.com>
2024-10-14 15:31:19 +0100

    Improve landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
