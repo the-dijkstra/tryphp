8579b1996e4460d308fd690371f6612132cd2df0
Mohammed Elhaouari <m@codiume.com>
2024-10-15 11:01:59 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
-	integrations: [tailwind()],
+	site: "https://tryphp.dev",
+	integrations: [tailwind(), sitemap()],
 });
