3441825b46dbc6bf6db02ae141beb68044e30f02
Mohammed Elhaouari <m@codiume.com>
2024-10-27 16:45:51 +0100

    Fix sitemap

--- .github/ISSUE_TEMPLATE/bug_report.md
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -12,6 +12,7 @@ A clear and concise description of what the bug is.
 
 **To Reproduce**
 Steps to reproduce the behavior:
+
 1. Go to '...'
 2. Run '....'
 3. See error
@@ -23,8 +24,9 @@ A clear and concise description of what you expected to happen.
 If applicable, add screenshots to help explain your problem.
 
 **Desktop (please complete the following information):**
- - OS: [e.g. iOS]
- - Version [e.g. 22]
+
+- OS: [e.g. iOS]
+- Version [e.g. 22]
 
 **Additional context**
 Add any other context about the problem here.
--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,9 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
-const base = "https://tryphp.dev";
-
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: base,
-  integrations: [
-    tailwind(),
-    sitemap({
-      customPages: [
-        `${base}/install.sh`,
-        `${base}/7.4/install.sh`,
-        `${base}/8.1/install.sh`,
-        `${base}/8.2/install.sh`,
-        `${base}/8.3/install.sh`,
-      ],
-    }),
-  ],
+  site: "https://tryphp.dev",
+  integrations: [tailwind(), sitemap()],
 });
